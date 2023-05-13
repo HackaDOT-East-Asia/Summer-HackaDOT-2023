2885becce8a3c66ffda496fe0b55fdbbeaa5e9ce
bryant0437 <w071a05@gmail.com>
2023-05-13 16:23:20 +0800

    commit test 5

--- teams/topic4-MoonFarm/src/moon-farm/src/App.tsx
+++ teams/topic4-MoonFarm/src/moon-farm/src/App.tsx
@@ -5,18 +5,8 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD:teams/topic4-MoonFarm/src/moon-farm/src/App.js
-      <header className="Moon Farm">
-        <h1 className="text-3xl font-bold underline">
-          Moon Farm
-        </h1>
-        <p>
-          commit test
-        </p>
-=======
       <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Moon Farm</h1>
->>>>>>> 61b626ddabedd53dd7bcc0c87f9d917061cabd37:teams/topic4-MoonFarm/src/moon-farm/src/App.tsx
+        <h1 className="text-3xl font-bold underline">Moon Farm1</h1>
       </header>
     </div>
   );
