61b626ddabedd53dd7bcc0c87f9d917061cabd37
tony <mamanto163@gmail.com>
2023-05-13 15:39:22 +0800

    v0.1.1 React Typescript TailwindCSS

--- teams/topic4-MoonFarm/src/moon-farm/src/App.tsx
+++ teams/topic4-MoonFarm/src/moon-farm/src/App.tsx
@@ -1,13 +1,12 @@
+import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="Moon Farm">
-        <h1 className="text-3xl font-bold underline">
-          Moon Farm
-        </h1>
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">Moon Farm</h1>
       </header>
     </div>
   );
