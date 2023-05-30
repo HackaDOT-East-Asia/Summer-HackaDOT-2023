cd15b22cae48180deea6103f5cfcae82e2aa5e15
humancontroller <131235865+humancontroller@users.noreply.github.com>
2023-05-30 21:34:10 +0900

    Update shutdown.py

--- teams/topic1-NFT_Camera/shutdown.py
+++ teams/topic1-NFT_Camera/shutdown.py
@@ -1,22 +1,18 @@
-import RPi.GPIO as GPIO		# RPi.GPIOモジュールを使用
+import RPi.GPIO as GPIO
 import time
 import os
 
-gpio_shutdown = 16	# GNDに接続でシャットダウン
+gpio_shutdown = 16
 
-# GPIO番号指定の準備
-GPIO.setmode(GPIO.BCM)	# GPIO○○でGPIO番号指定するモード
-			# ピン番号で指定する場合はGPIO.BOARD
+GPIO.setmode(GPIO.BCM)
 
-# GPIOを入力、プルアップに設定
 GPIO.setup(gpio_shutdown, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
 
-    # シャットダウン
     sw = GPIO.input(gpio_shutdown)
 
     if 0==sw:
         os.system("sh shutdown.sh")
 
-    time.sleep(0.01)	# 10ミリ秒待機
+    time.sleep(0.01)
